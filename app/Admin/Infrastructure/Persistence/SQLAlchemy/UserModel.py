43e3409c630cd8363cbe7da0ca1aa2978d58c8fd
Javier Rodríguez <javier.rodriguez@create-store.com>
2025-12-02 16:00:30 +0100

    ADD: Unique keys in fields

--- app/Admin/Infrastructure/Persistence/SQLAlchemy/UserModel.py
+++ app/Admin/Infrastructure/Persistence/SQLAlchemy/UserModel.py
@@ -6,8 +6,8 @@ class UserModel(db.Model):
     __tablename__ = 'users'
     
     id = Column(String(36), unique=True, nullable=False, primary_key=True)
-    username = Column(String(250), unique=False, nullable=False)
-    email = Column(String(250), unique=False, nullable=False)
+    username = Column(String(250), unique=True, nullable=False)
+    email = Column(String(250), unique=True, nullable=False)
     
     def __init__(
         self,
