75f93058fe4a1686bb0f1ad738e5b9357b931c6f
Javier Rodríguez <javier.rodriguez@create-store.com>
2025-12-10 13:25:14 +0100

    FIX: deleted deprecated stuff

--- app/config/container.py
+++ app/config/container.py
@@ -5,10 +5,6 @@ Incluye soporte completo para CQRS con Command Bus y Query Bus.
 """
 
 from dependency_injector import containers, providers
-from Infrastructure.Repository.HelloWorldWriteRepository import HelloWorldWriteRepository
-from Infrastructure.Repository.HelloWorldReadRepository import HelloWorldReadRepository
-from Infrastructure.Repository.ShowsAPIRepository import ShowsAPIRepository
-from Application.MoviesService import MoviesService
 from Admin.Infrastructure.Repository.UserWriteRepository import UserWriteRepository
 
 # CQRS - Command Bus y Query Bus
@@ -16,12 +12,6 @@ from Shared.Application.CommandBus import CommandBus
 from Shared.Application.QueryBus import QueryBus
 
 # Commands y Command Handlers
-from Application.Commands.CreateHelloWorldCommand import CreateHelloWorldCommand
-from Application.Commands.UpdateHelloWorldCommand import UpdateHelloWorldCommand
-from Application.Commands.DeleteHelloWorldCommand import DeleteHelloWorldCommand
-from Application.CommandHandlers.CreateHelloWorldHandler import CreateHelloWorldHandler
-from Application.CommandHandlers.UpdateHelloWorldHandler import UpdateHelloWorldHandler
-from Application.CommandHandlers.DeleteHelloWorldHandler import DeleteHelloWorldHandler
 from Admin.Application.Commands.CreateUserCommand import CreateUserCommand
 from Admin.Application.Commands.UpdateUserCommand import UpdateUserCommand
 from Admin.Application.Commands.DeleteUserCommand import DeleteUserCommand
@@ -30,25 +20,11 @@ from Admin.Application.Commands.UpdateUserAddressCommand import UpdateUserAddres
 from Admin.Application.Commands.RemoveUserAddressCommand import RemoveUserAddressCommand
 
 # Queries y Query Handlers
-from Application.Queries.GetAllHelloWorldQuery import GetAllHelloWorldQuery
-from Application.Queries.GetHelloWorldByIdQuery import GetHelloWorldByIdQuery
-from Application.Queries.SearchHelloWorldQuery import SearchHelloWorldQuery
-from Application.QueryHandlers.GetAllHelloWorldHandler import GetAllHelloWorldHandler
-from Application.QueryHandlers.GetHelloWorldByIdHandler import GetHelloWorldByIdHandler
-from Application.QueryHandlers.SearchHelloWorldHandler import SearchHelloWorldHandler
 from Admin.Application.Queries.GetUserByIdQuery import GetUserByIdQuery
 from Admin.Application.Queries.GetAllUsersQuery import GetAllUsersQuery
 
-# Use Cases - Shows
-from Application.UseCases.Shows.SearchShowsUseCase import SearchShowsUseCase
-from Application.UseCases.Shows.GetShowByIdUseCase import GetShowByIdUseCase
-
 # Event System
 from Shared.Infrastructure.Events.EventDispatcher import EventDispatcher
-from Application.EventHandlers.HelloWorldCreatedLogger import HelloWorldCreatedLogger
-from Application.EventHandlers.HelloWorldDeletedLogger import HelloWorldDeletedLogger
-from Application.EventHandlers.HelloWorldUpdatedLogger import HelloWorldUpdatedLogger
-from Infrastructure.Projections.HelloWorldProjection import HelloWorldProjection
 from Admin.Application.CommandHandlers.CreateUserHander import CreateUserHander
 from Admin.Application.CommandHandlers.UpdateUserHandler import UpdateUserHandler
 from Admin.Application.CommandHandlers.DeleteUserHandler import DeleteUserHandler
@@ -83,38 +59,8 @@ class Container(containers.DeclarativeContainer):
         EventDispatcher
     )
 
-    # Event Handlers
-    hello_world_created_logger = providers.Factory(
-        HelloWorldCreatedLogger
-    )
-
-    hello_world_deleted_logger = providers.Factory(
-        HelloWorldDeletedLogger
-    )
-
-    hello_world_updated_logger = providers.Factory(
-        HelloWorldUpdatedLogger
-    )
-
     # ========== REPOSITORIES ==========
-
-    # Write Repository (para comandos)
-    hello_world_write_repository = providers.Factory(
-        HelloWorldWriteRepository
-    )
-
-    # Read Repository (para queries)
-    hello_world_read_repository = providers.Factory(
-        HelloWorldReadRepository
-    )
-
-    # Shows Repository (API Externa)
-    shows_repository = providers.Factory(
-        ShowsAPIRepository,
-        api_host=config.stream_availability_host,
-        api_key=config.stream_availability_key
-    )
-    
+  
     admin_user_write_repository = providers.Factory(
         UserWriteRepository
     )
@@ -125,26 +71,7 @@ class Container(containers.DeclarativeContainer):
 
 
     # ========== CQRS - COMMAND HANDLERS ==========
-
-    create_hello_world_command_handler = providers.Factory(
-        CreateHelloWorldHandler,
-        write_repository=hello_world_write_repository,
-        event_dispatcher=event_dispatcher
-    )
-
-    update_hello_world_command_handler = providers.Factory(
-        UpdateHelloWorldHandler,
-        write_repository=hello_world_write_repository,
-        event_dispatcher=event_dispatcher
-    )
-
-    delete_hello_world_command_handler = providers.Factory(
-        DeleteHelloWorldHandler,
-        write_repository=hello_world_write_repository,
-        read_repository=hello_world_read_repository,
-        event_dispatcher=event_dispatcher
-    )
-    
+ 
     create_user_command_handler = providers.Factory(
         CreateUserHander,
         write_repository=admin_user_write_repository,
@@ -183,22 +110,7 @@ class Container(containers.DeclarativeContainer):
     )
 
     # ========== CQRS - QUERY HANDLERS ==========
-
-    get_all_hello_world_query_handler = providers.Factory(
-        GetAllHelloWorldHandler,
-        read_repository=hello_world_read_repository
-    )
-
-    get_hello_world_by_id_query_handler = providers.Factory(
-        GetHelloWorldByIdHandler,
-        read_repository=hello_world_read_repository
-    )
-
-    search_hello_world_query_handler = providers.Factory(
-        SearchHelloWorldHandler,
-        read_repository=hello_world_read_repository
-    )
-    
+   
     get_user_by_id_query_handler = providers.Factory(
         GetUserByIdHandler,
         read_repository=admin_user_read_repository
@@ -218,34 +130,6 @@ class Container(containers.DeclarativeContainer):
     query_bus = providers.Singleton(
         QueryBus
     )
-
-    # ========== PROJECTIONS ==========
-
-    hello_world_projection = providers.Factory(
-        HelloWorldProjection,
-        read_repository=hello_world_read_repository
-    )
-
-    # ========== SERVICES ==========
-
-    # Movies Service
-    movies_service = providers.Factory(
-        MoviesService,
-        repository=shows_repository
-    )
-
-    # ========== USE CASES - SHOWS ==========
-
-    search_shows_use_case = providers.Factory(
-        SearchShowsUseCase,
-        repository=shows_repository
-    )
-
-    get_show_by_id_use_case = providers.Factory(
-        GetShowByIdUseCase,
-        repository=shows_repository
-    )
-
 
 def init_container(app) -> Container:
     """
@@ -288,13 +172,7 @@ def _register_event_handlers(container: Container) -> None:
     """
     dispatcher = container.event_dispatcher()
 
-    # Suscribir handlers de HelloWorld
-    dispatcher.subscribe(container.hello_world_created_logger())
-    dispatcher.subscribe(container.hello_world_deleted_logger())
-    dispatcher.subscribe(container.hello_world_updated_logger())
 
-    # Suscribir projection para actualizar read models
-    dispatcher.subscribe(container.hello_world_projection())
 
 
 def _register_command_handlers(container: Container) -> None:
@@ -307,18 +185,6 @@ def _register_command_handlers(container: Container) -> None:
     command_bus = container.command_bus()
 
     # Registrar handlers de comandos
-    command_bus.register(
-        CreateHelloWorldCommand,
-        container.create_hello_world_command_handler()
-    )
-    command_bus.register(
-        UpdateHelloWorldCommand,
-        container.update_hello_world_command_handler()
-    )
-    command_bus.register(
-        DeleteHelloWorldCommand,
-        container.delete_hello_world_command_handler()
-    )
     command_bus.register(
         CreateUserCommand,
         container.create_user_command_handler()
@@ -355,18 +221,6 @@ def _register_query_handlers(container: Container) -> None:
     query_bus = container.query_bus()
 
     # Registrar handlers de queries
-    query_bus.register(
-        GetAllHelloWorldQuery,
-        container.get_all_hello_world_query_handler()
-    )
-    query_bus.register(
-        GetHelloWorldByIdQuery,
-        container.get_hello_world_by_id_query_handler()
-    )
-    query_bus.register(
-        SearchHelloWorldQuery,
-        container.search_hello_world_query_handler()
-    )
     query_bus.register(
         GetUserByIdQuery,
         container.get_user_by_id_query_handler()
