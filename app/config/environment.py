492cf4c18a76f654aa08e7f36c9d60588309091d
Javier Rodríguez <javier.rodriguez@create-store.com>
2025-12-11 13:20:26 +0100

    ADD: improved exception handling

--- app/config/environment.py
+++ app/config/environment.py
@@ -5,9 +5,5 @@ def configureEnvironment(app: Flask):
     app.config.update(
         STREAM_AVAILABILITY_HOST = environ.get('STREAM_AVAILABILITY_HOST'),
         STREAM_AVAILABILITY_KEY = environ.get('STREAM_AVAILABILITY_KEY'),
-        RABBITMQ_USER = environ.get('RABBITMQ_USER'),
-        RABBITMQ_PASS = environ.get('RABBITMQ_PASS'),
-        RABBITMQ_HOST = environ.get('RABBITMQ_HOST'),
-        RABBITMQ_PORT = environ.get('RABBITMQ_PORT'),
-        RABBITMQ_VHOST = environ.get('RABBITMQ_VHOST'),
+        ENVIRONMENT = environ.get('ENVIRONMENT'),
     )
--- app/config/exceptionHanlder.py
+++ app/config/exceptionHanlder.py
@@ -1,16 +1,24 @@
 from flask import Flask
 import traceback
+from os import environ
 
 
 def exceptionHandler(app: Flask):
     @app.errorhandler(Exception)
-    def handle_exception(e):
-        exceptionCode = e.code if hasattr(e, 'code') else 500
+    def handle_exception(e):  
+        exceptionCode = 500
+        if hasattr(e, 'code') and isinstance(e.code, int) and 100 <= e.code < 600:
+            exceptionCode = e.code
+        
         exceptionMessage = str(e)
+        
         response = {
-            "error": exceptionMessage,
-            "code": exceptionCode,
-            "traceback": traceback.format_exc()
-        }
+                "error": exceptionMessage,
+                "code": exceptionCode,
+            }
+        
+        if environ.get('ENVIRONMENT') == 'dev':
+            response["traceback"] = traceback.format_exc()
+        
         app.logger.error(exceptionMessage)
         return response, exceptionCode
